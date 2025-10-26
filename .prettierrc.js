b01868c16c052bb2ab8c555a5788b19db315bc0f
Сергей Кондратьев <s.kandratsyeu@gmail.com>
2025-10-27 00:42:13 +0300

    app infrastructure

---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+/* jshint ignore:start */
+module.exports = {
+    // Base from @tinkoff/prettier-config (v5.0.0)
+    singleQuote: true,
+    trailingComma: 'es5',
+    arrowParens: 'always',
+
+    // Project-specific overrides
+    tabWidth: 4,
+    useTabs: true,
+    semi: true,
+    bracketSpacing: true,
+    bracketSameLine: false,
+    htmlWhitespaceSensitivity: 'css',
+    singleAttributePerLine: true,
+    endOfLine: 'crlf',
+};
+/* jshint ignore:end */
